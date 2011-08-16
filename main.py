e20fcb228ec5c94bea22b4894b2ebfec20a0b026
lyxint <lyxint@gmail.com>
2011-08-16 22:03:09 +0800

    feeds2all

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python
 #coding: utf8
+import conf
+from lib import *
+from time import sleep, gmtime
+from pub2all import pub2all
 
 def twitter2all():
-    from time import sleep
-    from pub2all import pub2all
-    from conf import twitter_user, exclude
-    from lib import load_prev_time
     from twitter import get_twitter_status
     
-    prevtime = load_prev_time(twitter_user)
-    statuses = get_twitter_status(twitter_user, prevtime)
+    prevtime = load_prev_time(conf.twitter_user)
+    statuses = get_twitter_status(conf.twitter_user, prevtime)
     
     for status, pubdate in statuses:
         if status[0] in exclude:
             continue
         print pubdate, status
-        pub2all(status, pubdate)
+        if pub2all(status):
+            save_prev_time(conf.twitter_user, pubdate)
         sleep(10)
-
+        
+def feeds2all():
+    from rss import get_rss_entries
+    
+    lasttimes = loadfrom('rss_lasttimes')
+    if lasttimes is None:
+        lasttimes = {}
+        
+    for url in feeds:
+        lasttime = lasttimes.get(url, gmtime(-3600*24))
+        statuses = get_rss_entries(url, lasttime)
+        maxtime = lasttime
+        
+        for status, publishtime in statuses:
+            print publishtime, status
+            if pub2all(status):
+                if maxtime < publishtime:
+                    maxtime = publishtime
+            
+        lasttimes[url] = maxtime
+        dumpto('rss_lasttimes', lasttimes)
 
 if __name__ == '__main__':
     twitter2all()
+    feeds2all()
--- pub2all.py
+++ pub2all.py
@@ -3,7 +3,7 @@
 
 from lib import *
 
-def pub2all(status, savetime=False):
+def pub2all(status):
     from renren import pub2renren
     from sina import pub2sina
     from douban import pub2douban
@@ -12,53 +12,51 @@ def pub2all(status, savetime=False):
     from fanfou import pub2fanfou
     import conf
     
+    flag = False
+    
     if conf.renren_user and conf.renren_passwd:
         try:
             pub2renren(conf.renren_user, conf.renren_passwd, status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2renren error: %s' % str(e))
         
     if conf.sina_user and conf.sina_passwd:
         try:
             pub2sina(status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2sina error: %s' % str(e))
             
     if conf.facebook_user and conf.facebook_passwd:
         try:
             pub2facebook(conf.facebook_user, conf.facebook_passwd, status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2facebook error: %s' % str(e))
             
     if conf.douban_user and conf.douban_passwd:
         try:
             pub2douban(conf.douban_user, conf.douban_passwd, status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2douban error: %s' % str(e))
 
     if conf.qq_user and conf.qq_passwd:
         try:
             pub2qq(status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2qq error: %s' % str(e))
     
     if conf.fanfou_user and conf.fanfou_passwd:
         try:
             pub2fanfou(conf.fanfou_user, conf.fanfou_passwd, status)
-            if savetime:
-                save_prev_time(conf.twitter_user, savetime)
+            flag = True
         except Exception, e:
             log('pub2fanfou error: %s' % str(e))
+    
+    return flag
 
             
 if __name__ == '__main__':
