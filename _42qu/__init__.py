c41c168c60c042a57fddb90d3a87e94152f47070
lyxint <lyxint@gmail.com>
2012-01-10 15:31:30 +0800

    42qu use urlfetch instead of pycurl

--- _42qu/__init__.py
+++ _42qu/__init__.py
@@ -1,67 +1,80 @@
 #coding: utf8
-import sys
-sys.path.insert(0, '..')
-from bc import BC
-import re
-import urllib
-import pycurl
 
-class _42qu(BC):
+from urlfetch import fetch, sc2cs
+import re
+from urlparse import urljoin
+
+class _42qu(object):
     
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.cookie_file = '/dev/null'
-        BC.__init__(self)
-        self.reset()
+        self.cookies = ''
         
     def login(self):
-        b, c = self.reset()
-        c.setopt(pycurl.URL, "http://42qu.com/auth/login")
-        c.setopt(pycurl.REFERER, 'http://42qu.com/')
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.perform()
-        data = b.getvalue()
-        xsrf = re.search('''name="_xsrf".*?value="(.*?)"''', data).group(1)
-        self.xsrf = xsrf
+        response = fetch(
+            "http://42qu.com/auth/login",
+            headers = {
+                'Referer': 'http://42qu.com/',
+                'User-Agent': 'Opera/9.60',
+            }
+        )
+        self.xsrf = re.search(
+            '''name="_xsrf".*?value="(.*?)"''',
+            response.body
+        ).group(1)
+        set_cookie = response.getheader('Set-Cookie')
+        self.cookies = sc2cs(set_cookie)
+        
+        response =fetch(
+            "http://42qu.com/auth/login",
+            data = {
+                '_xsrf': self.xsrf,
+                'mail': self.username,
+                'password': self.password,
+            },
+            headers = {
+                'Referer': 'http://42qu.com/auth/login',
+                'User-Agent': 'Opera/9.60',
+                'Cookie': self.cookies,
+            }
+        )
 
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://42qu.com/auth/login")
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            '_xsrf': xsrf,
-            'mail': self.username,
-            'password': self.password,
-        }))
-        try:
-            c.perform()
-        except pycurl.error, e:
-            if e.args[0] != 47: raise
-        self.eurl = c.getinfo(pycurl.EFFECTIVE_URL) 
-        self.eurl = self.eurl.replace('42qu.com//','')
-        #self.eurl = self.eurl.replace('/live', '')
-        return b.getvalue()
+        if response.getheader('location'):
+            self.eurl = urljoin(
+                "http://42qu.com/auth/login",
+                response.getheader('location')
+            )
+        else:
+            raise Exception("Unknown error")
+            
+        set_cookie = response.getheader('Set-Cookie')
+        self.cookies += "; " + sc2cs(set_cookie)
+        print self.cookies
+            
+        return response
 
     def update(self, status):
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, self.eurl.replace('/live', '/po/word'))
-        c.setopt(pycurl.REFERER, self.eurl)
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            'txt': status,
-            '_xsrf' : self.xsrf,
-        }))
-        c.perform()
-        return b.getvalue()
+        url = self.eurl.replace('/live', '/po/word')
+        response = fetch(
+            url,
+            data = {
+                'txt': status,
+                '_xsrf': self.xsrf,
+            },
+            headers = {
+                'Referer': self.eurl,
+                'Cookie': self.cookies,
+            }
+        )
+        return response
 
-_instance = None
+
 def pub2_42qu(username, password, status):
-    global _instance
-    if _instance is None:
-       o = _42qu(username, password)
-       o.login() 
-       _instance = o
-    o = _instance
+    o = _42qu(username, password)
+    o.login()
     o.update(status)
+
+if __name__ == '__main__':
+    import sys
+    pub2_42qu(*sys.argv[1:4])
