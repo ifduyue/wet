47df5d01affccdcd920d107a57d6243175bb4d79
lyxint <lyxint@gmail.com>
2012-01-10 16:03:48 +0800

    facebook use urlfetch instead of pycurl

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -1,83 +1,88 @@
 #coding: utf8
 
-import sys
-sys.path.insert(0, '..')
-from bc import BC
+from urlfetch import fetch, sc2cs
 import re
-import urllib
-import pycurl
 
-class Facebook(BC):
+class Facebook(object):
     
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.cookie_file = '/dev/null'
-        BC.__init__(self)
-        self.reset()
+        self.cookies = ''
         
-    def login(self): 
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://m.facebook.com/")
-        c.setopt(pycurl.REFERER, 'http://m.facebook.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.perform()
-        m = re.search('''name="post_form_id" value="([^"]+?)"''', b.getvalue())
+    def login(self):
+        response = fetch(
+            "http://m.facebook.com/"
+        )
+        
+        m = re.search('''name="post_form_id" value="([^"]+?)"''', response.body)
         self.post_form_id = m.group(1)
+        
+        response = fetch(
+            "https://www.facebook.com/login.php?m=m&refsrc=http%3A%2F%2Fm.facebook.com%2F&refid=0",
+            data = {
+                'lsd': 'off',
+                'charset_test': "€,´,€,´,水,Д,Є",
+                'version': '1',
+                'ajax': '1',
+                'width': '1280',
+                'pxr': '1',
+                'email': self.username,
+                'pass': self.password,
+                'submit': 'Log In',
+                'post_form_id': self.post_form_id,
+            },
+            headers = {
+                'Referer': 'http://m.facebook.com/',
+            }
+        )
 
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "https://www.facebook.com/login.php?m=m&refsrc=http%3A%2F%2Fm.facebook.com%2F&refid=0")
-        c.setopt(pycurl.REFERER, 'http://m.facebook.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            'lsd': 'off',
-            'charset_test': "€,´,€,´,水,Д,Є",
-            'version': '1',
-            'ajax': '1',
-            'width': '1280',
-            'pxr': '1',
-            'email': self.username,
-            'pass': self.password,
-            'submit': 'Log In',
-            'post_form_id': self.post_form_id,
-        }))
-        c.perform()
-        self.post_form_id = re.search('''name="post_form_id" value="([^"]+?)"''', b.getvalue()).group(1)
-        self.fb_dtsg = re.search('''name="fb_dtsg" value="([^"]+?)"''', b.getvalue()).group(1)
-        return b.getvalue()
+        set_cookie = response.getheader('Set-Cookie')
+        self.cookies = sc2cs(set_cookie)
+        
+        url = response.getheader('location')
+        response = fetch(
+            url,
+            headers = {
+                'Referer': 'http://m.facebook.com/',
+                'Cookie': self.cookies,
+            },
+        )
+
+        self.post_form_id = re.search(
+            '''name="post_form_id" value="([^"]+?)"''',
+            response.body
+        ).group(1)
+        self.fb_dtsg = re.search(
+            '''name="fb_dtsg" value="([^"]+?)"''',
+            response.body
+        ).group(1)
+
+        return response
         
     def update(self, status):
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://m.facebook.com/a/home.php?refid=7")
-        c.setopt(pycurl.REFERER, 'http://m.facebook.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            'post_form_id': self.post_form_id,
-            'charset_test': "€,´,€,´,水,Д,Є",
-            'status': status,
-            'fb_dtsg': self.fb_dtsg,
-            'update': 'Share',
-            'r2a': '1',
-        }))
-        c.perform()
-        return b.getvalue()
+        response = fetch(
+            "http://m.facebook.com/a/home.php?refid=7",
+            data = {
+                'post_form_id': self.post_form_id,
+                'charset_test': "€,´,€,´,水,Д,Є",
+                'status': status,
+                'fb_dtsg': self.fb_dtsg,
+                'update': 'Share',
+                'r2a': '1',
+            },
+            headers = {
+                'Referer': 'http://m.facebook.com/',
+                'Cookie': self.cookies,
+            }
+        )
+        return response
 
-_instance = None
 def pub2facebook(username, password, status):
-    global _instance
-    if _instance is None:
-        facebook = Facebook(username, password)
-        facebook.login()
-        _instance = facebook
-    facebook = _instance
+    facebook = Facebook(username, password)
+    facebook.login()
     facebook.update(status)
+
+if __name__ == '__main__':
+    import sys
+    pub2facebook(*sys.argv[1:4])
