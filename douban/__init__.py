42f65f9c9ec23deb39825aed618f4c72768cf685
lyxint <lyxint@gmail.com>
2012-01-10 15:49:43 +0800

    douban use urlfetch instead of pycurl

--- douban/__init__.py
+++ douban/__init__.py
@@ -1,70 +1,65 @@
 #coding: utf8
-import sys
-sys.path.insert(0, '..')
-from bc import BC
-import re
-import urllib
-import pycurl
 
-class Douban(BC):
+from urlfetch import fetch, sc2cs
+import re
+
+class Douban(object):
     
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.cookie_file = '/dev/null'
-        BC.__init__(self)
-        self.reset()
+        self.cookies = ''
         
     def login(self):
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://m.douban.com/")
-        c.setopt(pycurl.REFERER, 'http://m.douban.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.perform()
-        m = re.search('''href="/\?session=([^'"]+?)"''', b.getvalue())
-        self.session = m.group(1)
-        
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://m.douban.com/")
-        c.setopt(pycurl.REFERER, 'http://m.douban.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            'form_email': self.username,
-            'form_password': self.password,
-            'redir': '',
-            'user_login': '登录',
-            'session': self.session
-        }))
-        c.perform()
-        return b.getvalue()
+        response = fetch(
+            "http://m.douban.com/"
+        )
+
+        set_cookie = response.getheader('Set-Cookie')
+        self.cookies = sc2cs(set_cookie)
+
+        self.session = response.getheader('location').split('=', 1)[-1]
+
+        response = fetch(
+            "http://m.douban.com/",
+            data = {
+                'form_email': self.username,
+                'form_password': self.password,
+                'redir': '',
+                'user_login': '登录',
+                'session': self.session
+            },
+            headers = {
+                'Referer': 'http://m.douban.com/',
+                'Cookie': self.cookies,
+            }
+        )
+        set_cookie = response.getheader('Set-Cookie')
+        self.cookies += "; " + sc2cs(set_cookie)
+
+        return response
         
     def update(self, status):
-        b, c = self.reset()
-        c.setopt(pycurl.COOKIEJAR, self.cookie_file)
-        c.setopt(pycurl.URL, "http://m.douban.com/")
-        c.setopt(pycurl.REFERER, 'http://m.douban.com/')
-        c.setopt(pycurl.USERAGENT, 'Opera/9.60')
-        c.setopt(pycurl.POST, True)
-        c.setopt(pycurl.FOLLOWLOCATION, True)
-        c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-            'mb_text': status,
-            'session': self.session
-        }))
-        c.perform()
-        return b.getvalue()
+        response = fetch(
+            "http://m.douban.com/",
+            data = {
+                'mb_text': status,
+                'session': self.session
+            },
+            headers = {
+                'Referer': 'http://m.douban.com/',
+                'Cookie': self.cookies,
+            }
+        )
+        return response
         
-_instance = None
+
 def pub2douban(username, password, status):
-    global _instance
-    if _instance is None:
-        douban = Douban(username, password)
-        douban.login()
-        _instance = douban
-    douban = _instance
+    douban = Douban(username, password)
+    douban.login()
     douban.update(status)
     
+if __name__ == '__main__':
+    import sys
+    pub2douban(*sys.argv[1:4])
+
