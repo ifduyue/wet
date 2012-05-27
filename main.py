3d6835748b75a9ebd0463927236c18673a16383d
lyxint <lyxint@gmail.com>
2012-05-27 22:45:50 +0800

    optimize exclude and include

--- main.py
+++ main.py
@@ -6,6 +6,47 @@ from log import log
 from time import sleep, gmtime, strftime
 from pub2all import pub2all
 
+
+def can_pub(status):
+    to_exclude = to_include = False
+
+    for i in conf.exclude:
+        if isinstance(i, re_type):
+            if i.match(status):
+                to_exclude = True
+                break
+        elif isinstance(i, basestring):
+            if status.find(i) != -1:
+                to_exclude = True
+                break
+        else:
+            if i(status):
+                to_exclude = True
+                break
+
+    if to_exclude:
+        return False
+            
+    for i in conf.include:
+        if isinstance(i, re_type):
+            if i.match(status):
+                to_include = True
+                break
+        elif isinstance(i, basestring):
+            if status.find(i) != -1:
+                to_include = True
+                break
+        else:
+            if i(status):
+                to_include = True
+                break
+
+    if not to_include:
+        return False
+
+    return True
+
+
 def twitter2all():
     from twitter import get_twitter_status
     
@@ -22,40 +63,7 @@ def twitter2all():
     maxtime = prevtime
     for status, pubdate in statuses:
 
-        to_exclude = to_include = False
-
-        for i in conf.exclude:
-            if isinstance(i, re_type):
-                if i.match(status):
-                    to_exclude = True
-                    break
-            elif isinstance(i, basestring):
-                if status.find(i) != -1:
-                    to_exclude = True
-                    break
-            else:
-                if i(status):
-                    to_exclude = True
-                    break
-
-        if to_exclude:
-            continue
-                
-        for i in conf.include:
-            if isinstance(i, re_type):
-                if i.match(status):
-                    to_include = True
-                    break
-            elif isinstance(i, basestring):
-                if status.find(i) != -1:
-                    to_include = True
-                    break
-            else:
-                if i(status):
-                    to_include = True
-                    break
-
-        if not to_include:
+        if not can_pub(status):
             continue
 
         log("[publishing] %s : %s",
@@ -88,6 +96,9 @@ def feeds2all():
         
         for status, publishtime in statuses:
             status = format_ % status
+
+            if not can_pub(status):
+                continue
             
             log("[publishing] %s : %s",
                 strftime("%Y-%m-%d %H:%M:%S", publishtime),
