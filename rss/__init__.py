d1feef3f809f9a67bfa3ec4bfcd31704c61312c6
lyxint <lyxint@gmail.com>
2012-06-07 12:49:05 +0800

    more compatible get_rss_entries

--- rss/__init__.py
+++ rss/__init__.py
@@ -4,6 +4,7 @@
 def get_rss_entries(url, prevtime=None):
     import feedparser
     import sys
+    import time
     sys.path.insert(0, '..')
     from lib import mb_code, strip_tags
     
@@ -21,18 +22,31 @@ def get_rss_entries(url, prevtime=None):
             content = strip_tags(content) 
         except:
             content = ''
-        try:
-            publishtime = e.published_parsed 
-        except:
-            publishtime = e.updated_parsed
+
+        publishtime = getattr(
+            e, 'published_parsed', 
+            getattr(
+                e, 'updated_parsed', 
+                time.strptime('1988-10-25', '%Y-%m-%d')
+            )
+        )
+
         msg = {
             'title': title, 
             'url': href,
             'content': content,
+            'entry': e,
         }
+        for i in e:
+            msg[i] = mb_code(e[i]) if isinstance(e[i], basestring) else e[i]
         
         if prevtime is None or publishtime > prevtime:
             statuses.append((msg, publishtime))            
     
     return statuses
 
+
+if __name__ == '__main__':
+    import sys
+    for msg, publishtime in get_rss_entries(sys.argv[1]):
+        print msg['title'], msg['url'], msg['content']
