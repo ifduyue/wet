acef38d4a12040123e7ddd1708b0c1c32b98cebe
lyxint <lyxint@gmail.com>
2012-07-19 18:36:58 +0800

    v2ex

---
+++ v2ex/__init__.py
@@ -0,0 +1,39 @@
+#coding: utf8
+
+import urlfetch
+import re
+
+class V2ex(object):
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.session = urlfetch.Session(headers={'Referer': 'http://v2ex.com/'})
+        
+    def login(self):
+        self.session.post('http://v2ex.com/signin', data={'u': self.username, 'p': self.password})
+        print self.session.cookies
+        return 'auth' in self.session.cookies
+    
+    def update(self, title, content='', node="blog"):
+        r = self.session.post(
+            "http://v2ex.com/new/" + node,
+            data = {'title': title, 'content': content}
+        )
+        return r
+        
+
+_instance = None
+
+def pub2v2ex(username, password, status):
+    global _instance
+    if _instance is None:
+        v2ex = V2ex(username, password)
+        v2ex.login()
+        _instance = v2ex
+    _instance.update(status)
+
+if __name__ == '__main__':
+    import sys
+    pub2v2ex(*sys.argv[1:4])
+
